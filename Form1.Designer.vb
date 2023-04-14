6908bd6125c16ab809e70ac5342d98a8fef9f764
pman1 <pman1@DESKTOP-OUTCHQH>
2023-04-14 09:15:38 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.picBuilding = New System.Windows.Forms.PictureBox()
+        Me.radInchMet = New System.Windows.Forms.RadioButton()
+        Me.radMetInch = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtValue = New System.Windows.Forms.TextBox()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblAnswer = New System.Windows.Forms.Label()
+        CType(Me.picBuilding, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'picBuilding
+        '
+        Me.picBuilding.Image = Global.Building_Plans_Conversion.My.Resources.Resources.building
+        Me.picBuilding.Location = New System.Drawing.Point(16, 15)
+        Me.picBuilding.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.picBuilding.Name = "picBuilding"
+        Me.picBuilding.Size = New System.Drawing.Size(349, 236)
+        Me.picBuilding.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBuilding.TabIndex = 0
+        Me.picBuilding.TabStop = False
+        '
+        'radInchMet
+        '
+        Me.radInchMet.AutoSize = True
+        Me.radInchMet.Checked = True
+        Me.radInchMet.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.radInchMet.ForeColor = System.Drawing.Color.White
+        Me.radInchMet.Location = New System.Drawing.Point(8, 47)
+        Me.radInchMet.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.radInchMet.Name = "radInchMet"
+        Me.radInchMet.Size = New System.Drawing.Size(169, 28)
+        Me.radInchMet.TabIndex = 1
+        Me.radInchMet.TabStop = True
+        Me.radInchMet.Text = "Inches to Meters"
+        Me.radInchMet.UseVisualStyleBackColor = True
+        '
+        'radMetInch
+        '
+        Me.radMetInch.AutoSize = True
+        Me.radMetInch.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.radMetInch.ForeColor = System.Drawing.Color.White
+        Me.radMetInch.Location = New System.Drawing.Point(8, 81)
+        Me.radMetInch.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.radMetInch.Name = "radMetInch"
+        Me.radMetInch.Size = New System.Drawing.Size(169, 28)
+        Me.radMetInch.TabIndex = 2
+        Me.radMetInch.Text = "Meters to Inches"
+        Me.radMetInch.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(405, 44)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(202, 31)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Converter App"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(408, 113)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(180, 24)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Enter a value and "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(408, 135)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(188, 24)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "choose conversion"
+        '
+        'txtValue
+        '
+        Me.txtValue.BackColor = System.Drawing.Color.Indigo
+        Me.txtValue.ForeColor = System.Drawing.Color.White
+        Me.txtValue.Location = New System.Drawing.Point(653, 114)
+        Me.txtValue.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtValue.Name = "txtValue"
+        Me.txtValue.Size = New System.Drawing.Size(132, 22)
+        Me.txtValue.TabIndex = 6
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnConvert.Location = New System.Drawing.Point(89, 417)
+        Me.btnConvert.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(188, 55)
+        Me.btnConvert.TabIndex = 7
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(332, 417)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(188, 55)
+        Me.btnClear.TabIndex = 8
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(579, 417)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(188, 55)
+        Me.btnExit.TabIndex = 9
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Indigo
+        Me.GroupBox1.Controls.Add(Me.radMetInch)
+        Me.GroupBox1.Controls.Add(Me.radInchMet)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.ForeColor = System.Drawing.Color.White
+        Me.GroupBox1.Location = New System.Drawing.Point(465, 210)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox1.Size = New System.Drawing.Size(321, 126)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Convert Measurement"
+        '
+        'lblAnswer
+        '
+        Me.lblAnswer.AutoSize = True
+        Me.lblAnswer.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAnswer.Location = New System.Drawing.Point(468, 369)
+        Me.lblAnswer.Name = "lblAnswer"
+        Me.lblAnswer.Size = New System.Drawing.Size(252, 29)
+        Me.lblAnswer.TabIndex = 14
+        Me.lblAnswer.Text = "# meters is # inches "
+        Me.lblAnswer.UseMnemonic = False
+        Me.lblAnswer.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.MediumPurple
+        Me.ClientSize = New System.Drawing.Size(839, 554)
+        Me.Controls.Add(Me.lblAnswer)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.txtValue)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.picBuilding)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Form1"
+        Me.Text = "Building Plans Conversion"
+        CType(Me.picBuilding, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picBuilding As PictureBox
+    Friend WithEvents radInchMet As RadioButton
+    Friend WithEvents radMetInch As RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtValue As TextBox
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents lblAnswer As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        'Declaration section
+        Dim intNumber As Integer = 0
+        Dim decTotal As Decimal
+        Dim decInch As Decimal = 0.0245D
+        Dim decMeter As Decimal = 39.3701D
+
+        lblAnswer.Visible = True
+
+        If IsNumeric(txtValue.Text) Then
+            intNumber = Convert.ToInt32(txtValue.Text)
+            If intNumber > 0 Then
+                If radInchMet.Checked Then
+                    decTotal = (intNumber * decInch)
+                    lblAnswer.Text = intNumber & " inches " & decTotal & " meters "
+                ElseIf radMetInch.Checked Then
+                    decTotal = (intNumber * decMeter)
+                    lblAnswer.Text = intNumber & " meters " & decTotal & " inches "
+
+                End If
+
+            Else
+                'Display error message if user entered a negative number 
+                MsgBox("Enter a positive number ", , "Invalid number error")
+
+            End If
+        Else
+            'Display error message if user entered a nonnumeric value 
+            MsgBox("Enter a reasonable number ", , "Invalid Entry error")
+        End If
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtValue.Clear()
+        radInchMet.Checked = True
+        radMetInch.Checked = False
+        txtValue.Focus()
+        lblAnswer.Text = ""
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+End Class
